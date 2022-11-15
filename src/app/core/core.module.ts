7e7e15abd3542999ea8e8b3e46d014866e0ec64d
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2022-11-15 09:54:26 +0100

    fix: update styles

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [HeaderComponent, FooterComponent]
 })
