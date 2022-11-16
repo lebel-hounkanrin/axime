b782a9858327a521603dac93adcadcd367b39daf
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2022-11-16 11:12:05 +0100

    feat: add login and registration component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
@@ -11,7 +12,8 @@ import { RegistrationComponent } from './registration/registration.component';
     RegistrationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [LoginComponent]
 })
